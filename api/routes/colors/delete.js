0b7051dad599f1857593acf902644098bfccb802
mikachou <chou1005.tw@gmail.com>
2024-05-26 18:09:24 +0800

    fix deletion

--- api/routes/colors/delete.js
+++ api/routes/colors/delete.js
@@ -3,11 +3,7 @@ import Color from "../../models/Color.js";
 const deleteColor = async (req, res, next) => {
   try {
     const colorId = req.params.colorId;
-    const color = await Color.findById(colorId).exec();
-    if (!color) {
-      return res.status(404).json({ error: "Color not found" });
-    }
-    await color.delete();
+    await Color.findByIdAndDelete(colorId).exec();
     res.send({ message: "Color deleted" });
   } catch (err) {
     next(err);
--- api/routes/products/delete.js
+++ api/routes/products/delete.js
@@ -3,11 +3,7 @@ import Product from "../../models/Product.js";
 const deleteProduct = async (req, res, next) => {
   try {
     const productId = req.params.productId;
-    const product = await Product.findById(productId).exec();
-    if (!product) {
-      return res.status(404).json({ error: "Product not found" });
-    }
-    await product.delete();
+    await Product.findByIdAndDelete(productId).exec();
     res.send({ message: "Product deleted" });
   } catch (err) {
     next(err);
