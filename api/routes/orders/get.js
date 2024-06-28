a52ef289fd499ff7661019799ec51c9d6a03fff8
mikachou <chou1005.tw@gmail.com>
2024-06-28 10:10:26 +0800

    adjust get order api

--- api/routes/orders/get.js
+++ api/routes/orders/get.js
@@ -1,22 +1,26 @@
 import Order from "../../models/Order.js";
+import Cart from "../../models/Cart.js";
 
 const getOrder = async (req, res, next) => {
   try {
     const userId = req.user.id;
     const orderId = req.params.orderId;
 
+    const cart = await Cart.findOne({ userId, orderId: orderId })
+      .populate("products.product")
+      .exec();
     const order = await Order.findById(orderId).exec();
     if (!order) {
       return res.status(404).json({ error: "Order not found" });
     }
 
-    if (order.userId.toString() !== userId) {
+    if (cart.userId.toString() !== userId) {
       return res
         .status(401)
         .json({ error: "You do not have access to this order" });
     }
 
-    res.send(order);
+    res.send({ order, cart });
   } catch (err) {
     next(err);
   }
